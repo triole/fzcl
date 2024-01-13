7ad684234b6c9c2671be2ee92cecbb4e624b7446
olaf michaelis <o.mic@web.de>
2024-01-13 22:09:36 +0100

    Add buld tags

--- src/main.go
+++ src/main.go
@@ -5,6 +5,13 @@ import (
 	"time"
 )
 
+var (
+	BUILDTAGS      string
+	appName        = "fzcl"
+	appDescription = "fuzzy clock"
+	appMainversion = "0.1"
+)
+
 func main() {
 	fmt.Println(fuzzy(time.Now()))
 }
